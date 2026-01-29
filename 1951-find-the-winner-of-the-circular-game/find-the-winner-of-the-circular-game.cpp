f4846a1062ec859470491989e9236330425789da
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-01-29 13:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.88%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
+    // int f(queue<int>& qu, int k) {
+    //     while (qu.size() != 1) {
+    //         for (int i = 0; i < k - 1; i++) {
+    //             int a = qu.front();
+    //             qu.pop();
+    //             qu.push(a);
+    //         }
+    //         qu.pop();
+    //     }
+    //     return qu.front();
+    // }
 
-    int f(queue<int>&qu,int k){
-        while(qu.size()!=1){
-            for(int i=0;i<k-1;i++){
-            int a = qu.front();
-            qu.pop();
-            qu.push(a);
-            }
-            qu.pop();
-        }
-        return qu.front();
 
-
+    int f(vector<int>&ele,int k,int i){
+        if(ele.size()==1) return ele[0];
+        int d =(i + k -1) % ele.size();
+        ele.erase(ele.begin() + d);
+        return f(ele,k,d);
     }
 
     int findTheWinner(int n, int k) {
-        queue<int>qu;
-        for(int i=1;i<=n;i++){
-            qu.push(i);
+        // queue<int> qu;
+        vector<int>ele;
+        for (int i = 1; i <= n; i++) {
+            ele.push_back(i);
         }
-        return f(qu,k);
 
+
+
+
+        return f(ele, k,0);
     }
 };
