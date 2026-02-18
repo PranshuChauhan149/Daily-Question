bd700c49c3a99abf1fbe68a33dc39c58fd6e1f1c
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-02-18 22:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (73.90%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,30 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root,vector<int>&ans){
-        if(root==NULL) return ;
-        inOrder(root->left,ans);
-        ans.push_back(root->val);
-        inOrder(root->right,ans);
-    }
-
-    bool check(vector<int>&ans){
-        for(int i=0;i<ans.size()-1;i++){
-            if(ans[i] >= ans[i+1]){
-                return false;
-            }
+    bool check(TreeNode* root, long  l, long r) {
+        if (root == NULL)
+            return true;
+        if (root->val <= l || root->val >= r) {
+            return false;
         }
-        return true;
+        return check(root->left, l, root->val) &&
+               check(root->right, root->val, r);
     }
 
-    bool isValidBST(TreeNode* root) {
-        vector<int>ans;
-        inOrder(root,ans);
-        return check(ans);
-    }
+    bool isValidBST(TreeNode* root) { return check(root, LONG_MIN, LONG_MAX); }
 };
