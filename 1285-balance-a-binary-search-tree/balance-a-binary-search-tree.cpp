c6a7837c6bb03ac73316172c0305935267a9db9f
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-02-18 11:32:13 +0530

    Time: 17 ms (37.53%) | Memory: 65.6 MB (61.19%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -6,34 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* make(int l, int r, vector<int>& ans) {
-        if (l > r)
-            return NULL;
-         int mid = l + (r - l) / 2;
-        TreeNode* root = new TreeNode(ans[mid]);
-        root->left = make(l, mid - 1, ans);
-        root->right = make(mid + 1, r, ans);
-        return root;
+
+    TreeNode* make(TreeNode* root,vector<int>&ans,int s,int e){
+        if(e<s) return NULL;
+        int mid = (s + e)/2;
+        TreeNode* r = new TreeNode(ans[mid]);
+        r->left = make(root,ans,s,mid-1);
+        r->right = make(root,ans,mid+1,e);
+        return r;
+
     }
 
-    void inOrder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL)
-            return ;
-        inOrder(root->left, ans);
+    void inOrder(TreeNode* root,vector<int>&ans){
+        if(root==NULL) return ;
+        inOrder(root->left,ans);
         ans.push_back(root->val);
-        inOrder(root->right, ans);
+        inOrder(root->right,ans);
     }
 
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> ans;
-        inOrder(root, ans);
-        return make(0,ans.size()-1,ans);
+        vector<int>ans;
+        inOrder(root,ans);
+        return make(root,ans,0,ans.size()-1);
 
+        
     }
 };
