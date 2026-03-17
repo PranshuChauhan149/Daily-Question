e34179ad38b343f9b63ffba86255a585936f1289
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-03-17 22:53:28 +0530

    Time: 108 ms (8.98%) | Memory: 18.5 MB (66.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,50 +10,38 @@
  */
 class Solution {
 public:
-    
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dm  = new ListNode(-1);
-        ListNode* temp = dm;
-
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+                temp = temp->next;
+            } else {
+                 temp->next = list2;
+                list2 = list2->next;
+                temp = temp->next;
+            }
+        }
         if(list1==NULL){
-            return list2;
-        }
-        if(list2==NULL){
-            return list1;
-        }
-    while(list1!=NULL && list2!=NULL){
-        if(list1->val<=list2->val){
-            temp->next = list1;
-            temp = list1;
-            list1=list1->next;
+            temp->next = list2;
         }
         else{
-            temp->next = list2;
-            temp=list2;
-            list2=list2->next;
+            temp->next = list1;
         }
+        
+        return dummy->next;
     }
-    while(list1!=NULL){
-         temp->next = list1;
-            temp = list1;
-            list1=list1->next;
-        }
 
-    while(list2!=NULL){
-         temp->next = list2;
-            temp = list2;
-            list2=list2->next;
-        }
-        return dm->next;
-
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
+        int n = lists.size();
+        if (n == 0)
+            return NULL;
         ListNode* ans = lists[0];
-        for(int i=1;i<lists.size();i++){
-         ans =    mergeTwoLists(ans,lists[i]);
+        for (int i = 1; i < n; i++) {
+            ans = merge(ans, lists[i]);
         }
         return ans;
-
     }
 };
