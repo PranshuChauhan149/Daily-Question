79da99759d2851e91fcd0ee371e7a6a738026805
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-03-23 22:45:08 +0530

    Time: 47 ms (33.34%) | Memory: 53.4 MB (10.35%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,59 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(vector<int>& coins, int amount, int i) {
+    int f(vector<int>& coins, int i, int amount, vector<vector<int>>& dp) {
         if (amount == 0)
             return 0;
         if (amount < 0)
-            return 1e8;
-        if (i == 0) {
-            if (amount % coins[0] == 0)
-                return amount / coins[0];
-            else {
-                return 1e8;
+            return 1e9;
+        if (i == coins.size() - 1) {
+            if (amount % coins[i] == 0) {
+                return amount / coins[i];
             }
+            return 1e9;
         }
+
         if (dp[i][amount] != -1)
             return dp[i][amount];
-        int ans = 1e8;
+        int p = 1e9;
+
         if (amount >= coins[i]) {
-            ans = 1 + f(coins, amount - coins[i], i);
+            p = 1 + f(coins, i, amount - coins[i], dp);
         }
-        int a = 0 + f(coins, amount, i - 1);
-        return dp[i][amount] = min(a, ans);
+        int r = f(coins, i + 1, amount, dp);
+        return dp[i][amount] = min(p, r);
     }
 
     int coinChange(vector<int>& coins, int amount) {
-
+        if (amount == 0)
+            return 0;
         int n = coins.size();
-        dp.resize(n + 1, vector<int>(amount + 1, 0));
-        // int a = f(coins, amount, n - 1);
-        // if (a == 1e8) {
-        //     return -1;
-        // }
+        vector<vector<int>> dp(n + 1, vector<int>( amount + 1, -1));
+        int ans = f(coins, 0, amount, dp);
 
-        for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0) {
-                dp[0][i] = i / coins[0];
-            } else {
-                dp[0][i] = 1e9;
-            }
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int a = 0; a <= amount; a++) {
-               
-                int ans = 1e9;
-                if (a >= coins[i]) {
-                    ans = 1 + dp[i][a - coins[i]];
-                }
-                int  b = dp[i-1][a];
-                 dp[i][a] = min(b, ans);
-            }
-        }
-    
-        int ans =  dp[n-1][amount];
-        if(ans==1e9) return -1;
-        return ans;
+        return ans == 1e9 ? -1 : ans;
     }
 };
