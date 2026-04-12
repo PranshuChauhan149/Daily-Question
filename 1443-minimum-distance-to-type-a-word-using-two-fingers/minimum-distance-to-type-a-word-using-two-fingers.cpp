ce24fe4165af9572ef441f4cc49321003a79f59f
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-04-12 23:05:20 +0530

    Time: 188 ms (11.65%) | Memory: 42.1 MB (27.31%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -3,45 +3,39 @@ public:
 
     pair<int,int> getCord(char ch){
         int c = ch - 'A';
-        return {c / 6, c % 6};
+        return {c/6, c%6};
     }
 
     int getDistance(int x1,int y1,int x2,int y2){
-        if(x1 == -1) return 0; // finger not used yet
+        if(x1 == -1) return 0; // unused finger
         return abs(x1 - x2) + abs(y1 - y2);
     }
 
-    int dp[301][27][27]; // memo table
+    map<tuple<int,int,int,int,int>, int> dp;
 
-    int solve(string &word,int i,int f1,int f2){
-        if(i == word.size()) return 0;
+    int solve(string &word,int i,int x1,int y1,int x2,int y2){
 
-        if(dp[i][f1+1][f2+1] != -1)
-            return dp[i][f1+1][f2+1];
+        if(i >= word.size()){
+            return 0;
+        }
 
-        int curr = word[i] - 'A';
+        auto key = make_tuple(i,x1,y1,x2,y2);
+        if(dp.count(key)) return dp[key];
 
         auto [x,y] = getCord(word[i]);
 
-        // finger1 move
-        int cost1 = getDistance(
-            (f1==-1 ? -1 : getCord(f1+'A').first),
-            (f1==-1 ? -1 : getCord(f1+'A').second),
-            x,y
-        ) + solve(word,i+1,curr,f2);
+        // move finger1
+        int moveF1 = getDistance(x1,y1,x,y) +
+                     solve(word,i+1,x,y,x2,y2);
 
-        // finger2 move
-        int cost2 = getDistance(
-            (f2==-1 ? -1 : getCord(f2+'A').first),
-            (f2==-1 ? -1 : getCord(f2+'A').second),
-            x,y
-        ) + solve(word,i+1,f1,curr);
+        // move finger2
+        int moveF2 = getDistance(x2,y2,x,y) +
+                     solve(word,i+1,x1,y1,x,y);
 
-        return dp[i][f1+1][f2+1] = min(cost1,cost2);
+        return dp[key] = min(moveF1,moveF2);
     }
 
     int minimumDistance(string word) {
-        memset(dp,-1,sizeof(dp));
-        return solve(word,0,-1,-1);
+        return solve(word,0,-1,-1,-1,-1);
     }
 };
