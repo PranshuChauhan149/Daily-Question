d6d930797a86ea77cfa653d2d4d41ff5e613abc9
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-05-17 16:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (86.83%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int sum =0;
         int tak = 0;
+        int cos = 0;
         int b = 0;
         int ans = 0;
-        for(int i=0;i<gas.size()-1;i++){
-            sum+=gas[i];
-            tak+=cost[i];
+        int n = gas.size();
+        for (int i = 0; i < n - 1; i++) {
+            tak += gas[i];
+            cos += cost[i];
             b += gas[i];
-             b = b -cost[i];
-            if(b<0){
-                ans = i+1;
+            b -= cost[i];
+            if (b < 0) {
+                ans = i + 1;
                 b =0;
             }
-
         }
-        sum+=gas[gas.size()-1];
-        tak+=cost[gas.size()-1];
-        if(sum<tak) return -1;
+        tak += gas[n - 1];
+        cos += cost[n - 1];
+
+        if (tak < cos)
+            return -1;
         return ans;
     }
 };
