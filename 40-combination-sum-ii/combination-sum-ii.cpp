32f11df6b3c5d32914a86d49ab9a7f0789dad312
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-05-25 22:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (82.01%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    set<vector<int>> st;
     void f(vector<int>& candidates, int i, int target, vector<int>& a,
            vector<vector<int>>& ans) {
-        
         if (target == 0) {
-      
-            if (!st.count(a)) {
-                ans.push_back(a);
-                st.insert(a);
-            }
+            ans.push_back(a);
             return;
         }
-
-if (i >= candidates.size())
+        if (i >= candidates.size()) {
             return;
-
+        }
         if (candidates[i] <= target) {
             a.push_back(candidates[i]);
             f(candidates, i + 1, target - candidates[i], a, ans);
             a.pop_back();
         }
         int idx = i+1;
-        while(idx<candidates.size() && candidates[idx]==candidates[i]) idx++;
+        while (idx<candidates.size() && candidates[idx] == candidates[i])
+            idx++;
         f(candidates, idx, target, a, ans);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> a;
-        vector<vector<int>> ans;
         sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> a;
         f(candidates, 0, target, a, ans);
         return ans;
     }
