b0d222e76d301de4e17b44bd9c19b443212b75f4
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-05-26 23:56:30 +0530

    Time: 34 ms (94.46%) | Memory: 62.6 MB (69.62%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-
-    void f(int idx,int n,int k,vector<vector<int>>&ans,vector<int>&a){
-        
-        if(a.size()==k){
+    void f(int n, int idx, int k, vector<vector<int>>& ans, vector<int>& a) {
+        if (a.size() == k) {
             ans.push_back(a);
-            return ;
+            return;
         }
-        for(int i = idx;i<=n;i++){
+        for (int i = idx; i <= n; i++) {
             a.push_back(i);
-            f(i+1,n,k,ans,a);
+            f(n, i + 1, k, ans, a);
             a.pop_back();
         }
     }
 
-
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
-        vector<int>a;
-        f(1,n,k,ans,a);
+        vector<int> a;
+        vector<vector<int>> ans;
+        f(n, 1, k, ans, a);
         return ans;
     }
 };
