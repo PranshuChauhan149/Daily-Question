96b56bc9e66d272743b82606a363390a461c11f8
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-06-26 14:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.29%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -6,29 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-
-    int height(TreeNode* p, TreeNode* q){
-        if(p==NULL && q!=NULL) return -1;
-        if(q==NULL && p!=NULL) return -1;
-        if(p==NULL || q==NULL) return 0;
-        if(p->val!=q->val){
-            return -1;
-        }
-        int left = height(p->left,q->left);
-        if(left==-1) return -1;
-        int right = height(p->right,q->right);
-        if(right==-1) return -1;
-        return p->val + max(left,right);
-        
-    }
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(height(p,q)==-1) return false;
-        return true;
+
+        if (p == NULL && q != NULL) {
+            return false;
+        }
+        if (q == NULL && p != NULL) {
+            return false;
+        }
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
